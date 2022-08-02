ff64e3578465b35bcea53e7335ef1e4859cc8019
Ovidiu <mateiovidiu10@gmail.com>
2022-08-02 16:55:48 +0300

    first commit

--- pages/events/past-events/[id].js
+++ pages/events/past-events/[id].js
@@ -1,6 +1,6 @@
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
-import { db } from "../../../components/firebase";
+import { db } from "../../../firebase";
 import { collection, getDocs } from "@firebase/firestore";
 import BuyForm from "../../../components/BuyForm";
 import { Splide, SplideSlide } from "@splidejs/react-splide";
