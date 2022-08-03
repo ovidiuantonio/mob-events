a7b3792bd938666f1e99c763417a666519446a2b
Ovidiu <mateiovidiu10@gmail.com>
2022-08-03 11:53:28 +0300

    stripe fix

--- pages/api/checkout/session.ts
+++ pages/api/checkout/session.ts
@@ -17,7 +17,7 @@ export default async function (req: NextApiRequest, res: NextApiResponse) {
       unit_amount: item.price * 100,
     },
     description: item.description,
-    metadata: {id: item.itemId},
+    metadata: {"id": `${item.itemId}`},
     quantity: 1,
   };
 
