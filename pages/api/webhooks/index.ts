b18586e280e79c368af9007b868bd45a48bf9d48
Ovidiu <mateiovidiu10@gmail.com>
2022-08-03 13:28:07 +0300

    stripe fix

--- pages/api/webhooks/index.ts
+++ pages/api/webhooks/index.ts
@@ -52,7 +52,7 @@ const webhookHandler = async (req: NextApiRequest, res: NextApiResponse) => {
             console.log(event.type)
         }
 
-        res.json({received: true, event: event.data.object.metadata.event})
+        res.json({received: true, event: event})
     } else {
         res.setHeader('Allow', 'POST')
         res.status(405).end('Method not allowed')
