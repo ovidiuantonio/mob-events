8e20ab971d1a17abd273ef1ae3a2cf9e7ed14c04
Ovidiu <mateiovidiu10@gmail.com>
2022-08-04 10:23:30 +0300

    webhook modif

--- pages/api/webhooks/index.ts
+++ pages/api/webhooks/index.ts
@@ -42,30 +42,33 @@ const webhookHandler = async (req: NextApiRequest, res: NextApiResponse) => {
 
     console.log("success", event.id);
 
-    if (event.type === "payment_intent.succeeded") {
+    // if (event.type === "payment_intent.succeeded") {
+    //   const paymentIntent = event.data.object as Stripe.PaymentIntent;
+    // } else if (event.type === "payment_intent.payment_failed") {
+    //   const paymentIntent = event.data.object as Stripe.PaymentIntent;
+    //   console.log(paymentIntent.last_payment_error?.message);
+    // } else if (event.type === "charge.succeeded") {
+    //   const charge = event.data.object as Stripe.Charge;
+    //   console.log(charge.id);
+    // } else {
+    //   console.log(event.type);
+    // }
+
+    if (event.type === "checkout.session.completed") {
       const paymentIntent = event.data.object as Stripe.PaymentIntent;
-    } else if (event.type === "payment_intent.payment_failed") {
-      const paymentIntent = event.data.object as Stripe.PaymentIntent;
-      console.log(paymentIntent.last_payment_error?.message);
-    } else if (event.type === "charge.succeeded") {
-      const charge = event.data.object as Stripe.Charge;
-      console.log(charge.id);
-    } else {
-      console.log(event.type);
+      const eventId = paymentIntent.metadata.event;
+      const email = paymentIntent.metadata.customer_email;
+      const name = paymentIntent.metadata.customer_name;
+      const tel = paymentIntent.metadata.customer_tel;
+      const id = paymentIntent.id;
+
+      await setDoc(doc(db, `tables-${eventId}`, `${id}`), {
+        name: name,
+        email: email,
+        tel: tel,
+      });
     }
 
-    const paymentIntent = event.data.object as Stripe.PaymentIntent;
-    const eventId = paymentIntent.metadata.event;
-    const email = paymentIntent.metadata.customer_email;
-    const name = paymentIntent.metadata.customer_name;
-    const tel = paymentIntent.metadata.customer_tel;
-    const id = paymentIntent.id;
-
-    await setDoc(doc(db, `tables-${eventId}`, `${id}`), {
-      name: name,
-      email: email,
-      tel: tel,
-    });
     res.status(200).json({ received: true });
   } else {
     res.setHeader("Allow", "POST");
