afe305e14bcf3bd410453b4cbe03f32e78a5556d
Ovidiu <mateiovidiu10@gmail.com>
2022-08-08 12:44:31 +0300

    got rid of progressbar

--- pages/_document.js
+++ pages/_document.js
@@ -14,15 +14,18 @@ export default function Document() {
               content="width=device-width, initial-scale=1"
             />
             <link rel="apple-touch-icon" href="/logo-mob-png.png"></link>
+            <link rel="preconnect" href="https://fonts.googleapis.com"></link>
+            <link
+              rel="preconnect"
+              href="https://fonts.gstatic.com"
+              crossorigin
+            ></link>
+            <link
+              href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&display=swap"
+              rel="stylesheet"
+            ></link>
             <meta name="theme-color" content="#feb700" />
             <meta name="description" content="Mob Events" />
-            <script
-              src="https://cdnjs.cloudflare.com/ajax/libs/nprogress/0.2.0/nprogress.min.js"
-              integrity="sha512-bUg5gaqBVaXIJNuebamJ6uex//mjxPk8kljQTdM1SwkNrQD7pjS+PerntUSD+QRWPNJ0tq54/x4zRV8bLrLhZg=="
-              crossorigin="anonymous"
-              referrerpolicy="no-referrer"
-            ></script>
-            <script src="nprogress.js"></script>
 
             <title>Mob Events</title>
           </head>
